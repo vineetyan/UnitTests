3463e0ea2384209252fece25460c57ba629825d0
Vineet Kumar <Vineet.Kumar@Radancy.com>
2023-09-05 16:36:15 +0530

    Unit Tests

---
+++ TestProj1/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace TestProj1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            UnitTestCases.Addition add = new UnitTestCases.Addition();
+            int x =add.AddInt(1, 2);
+            Assert.AreEqual(3, x);
+        }
+    }
+}
---
+++ UnitTestCases/Addition.cs
@@ -0,0 +1,16 @@
+﻿namespace UnitTestCases
+{
+    public class Addition
+    {
+        public int AddInt(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int AddFloat(int x, int y)
+        {
+            return x + y;
+        }
+
+    }
+}
